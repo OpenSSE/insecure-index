580115a4b813aedea981615b61ebac8365b24347
Raphael Bost <raphael_bost@alumni.brown.edu>
2019-03-16 18:23:26 +0100

    Add a benchmark for uniform distribution sampling. This allows us to easy compare the performance of the distributions.

--- benchmark/bench_zipf.cpp
+++ benchmark/bench_zipf.cpp
@@ -5,6 +5,19 @@
 namespace sse {
 
 
+static void Uniform_sample(benchmark::State& state)
+{
+    std::random_device                      rnd;
+    std::mt19937_64                         rnd_gen(rnd());
+    std::uniform_int_distribution<uint64_t> dist(0, state.range(0) - 1);
+
+
+    for (auto _ : state) {
+        benchmark::DoNotOptimize(dist(rnd_gen));
+    }
+    state.SetItemsProcessed(state.iterations());
+}
+
 static void Zipf_sample(benchmark::State& state)
 {
     std::random_device            rnd;
@@ -17,6 +30,15 @@ static void Zipf_sample(benchmark::State& state)
     }
     state.SetItemsProcessed(state.iterations());
 }
+
+
+BENCHMARK(Uniform_sample)
+    ->Arg(1000)
+    ->Arg(10000)
+    ->Arg(100000)
+    ->Arg(1000000)
+    ->Arg(10000000);
+
 
 BENCHMARK(Zipf_sample)
     ->Arg(1000)
