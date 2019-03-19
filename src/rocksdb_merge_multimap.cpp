8100c2ebca072a32fa455a2e348a68c7e5fc22a8
Raphael Bost <raphael_bost@alumni.brown.edu>
2019-03-19 23:35:58 +0100

    Optimization of the merge operator

--- src/rocksdb_merge_multimap.cpp
+++ src/rocksdb_merge_multimap.cpp
@@ -17,42 +17,116 @@ namespace insecure {
 class ResultListMergeOperator : public rocksdb::AssociativeMergeOperator
 {
 public:
+    // virtual bool Merge(const rocksdb::Slice& key,
+    //                    const rocksdb::Slice* existing_value,
+    //                    const rocksdb::Slice& value,
+    //                    std::string*          new_value,
+    //                    rocksdb::Logger*      logger) const override
+    // {
+    //     // assuming 0 if no existing value
+    //     std::vector<Index::document_type> existing_vec{};
+
+    //     if (existing_value) {
+    //         if (!Index::deserialize_document_list(*existing_value,
+    //                                               &existing_vec)) {
+    //             // if existing_value is corrupted, treat it as 0
+    //             rocksdb::Log(logger, "existing value corruption");
+    //             std::cerr << "corruption\n";
+    //             existing_vec = {};
+    //         }
+    //     }
+
+    //     std::vector<Index::document_type> deserialized_vec;
+    //     if (!Index::deserialize_document_list(value, &deserialized_vec)) {
+    //         // if operand is corrupted, treat it as 0
+    //         Log(logger, "operand value corruption");
+    //         std::cerr << "corruption\n";
+    //         deserialized_vec = {};
+    //     }
+    //     size_t n_existing = deserialized_vec.size();
+
+    //     deserialized_vec.resize(deserialized_vec.size() +
+    //     existing_vec.size()); std::copy(existing_vec.begin(),
+    //               existing_vec.end(),
+    //               deserialized_vec.begin() + n_existing);
+    //     // deserialized_vec.insert(
+    //     // deserialized_vec.end(), existing_vec.begin(), existing_vec.end());
+
+
+    //     *new_value = std::string(
+    //         reinterpret_cast<char*>(deserialized_vec.data()),
+    //         deserialized_vec.size() * sizeof(Index::document_type));
+    //     return true; // always return true for this, since we treat all
+    //     errors
+    //                  // as "zero".
+    // }
+
+
+    // virtual bool Merge(const rocksdb::Slice& key,
+    //                    const rocksdb::Slice* existing_value,
+    //                    const rocksdb::Slice& value,
+    //                    std::string*          new_value,
+    //                    rocksdb::Logger*      logger) const override
+    // {
+    //     if (!existing_value) {
+    //         *new_value = std::string(value.data(), value.size());
+    //         return true;
+    //     }
+
+    //     size_t concat_size = value.size();
+    //     if (existing_value) {
+    //         concat_size += existing_value->size();
+    //     }
+
+    //     std::vector<char> data(concat_size);
+    //     std::memcpy(data.data(), value.data(), value.size());
+
+    //     if (existing_value) {
+    //         std::memcpy(data.data() + value.size(),
+    //                     existing_value->data(),
+    //                     existing_value->size());
+    //     }
+
+    //     *new_value
+    //         = std::string(reinterpret_cast<char*>(data.data()), concat_size);
+    //     return true;
+    // }
+
     virtual bool Merge(const rocksdb::Slice& key,
                        const rocksdb::Slice* existing_value,
                        const rocksdb::Slice& value,
                        std::string*          new_value,
                        rocksdb::Logger*      logger) const override
     {
-        // assuming 0 if no existing value
-        std::vector<Index::document_type> existing_vec{};
+        if (!existing_value) {
+            *new_value = std::string(value.data(), value.size());
+            return true;
+        }
+        std::array<char, 1024> buffer;
+        char*                  data_ptr = nullptr;
 
+        size_t concat_size = value.size();
         if (existing_value) {
-            if (!Index::deserialize_document_list(*existing_value,
-                                                  &existing_vec)) {
-                // if existing_value is corrupted, treat it as 0
-                rocksdb::Log(logger, "existing value corruption");
-                std::cerr << "corruption\n";
-                existing_vec = {};
-            }
+            concat_size += existing_value->size();
         }
 
-        std::vector<Index::document_type> deserialized_vec;
-        if (!Index::deserialize_document_list(value, &deserialized_vec)) {
-            // if operand is corrupted, treat it as 0
-            Log(logger, "operand value corruption");
-            std::cerr << "corruption\n";
-            deserialized_vec = {};
+        if (concat_size > buffer.size()) {
+            data_ptr = new char[concat_size];
+        } else {
+            data_ptr = buffer.data();
+        }
+        std::memcpy(data_ptr, value.data(), value.size());
+        std::memcpy(data_ptr + value.size(),
+                    existing_value->data(),
+                    existing_value->size());
+
+        *new_value = std::string(data_ptr, concat_size);
+
+        if (concat_size > buffer.size()) {
+            delete[] data_ptr;
         }
 
-        deserialized_vec.resize(deserialized_vec.size() + existing_vec.size());
-        deserialized_vec.insert(
-            deserialized_vec.end(), existing_vec.begin(), existing_vec.end());
-
-        *new_value = std::string(
-            reinterpret_cast<char*>(deserialized_vec.data()),
-            deserialized_vec.size() * sizeof(Index::document_type));
-        return true; // always return true for this, since we treat all errors
-                     // as "zero".
+        return true;
     }
 
     virtual const char* Name() const override
